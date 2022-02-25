33339f83e795606e688a08a8fe8b058273ba44ce
Drathion <81193098+Drathion@users.noreply.github.com>
2022-02-25 01:29:20 -0500

    added line to rebuild VBox driver

--- rocky_vagranthost_setup.sh
+++ rocky_vagranthost_setup.sh
@@ -11,7 +11,7 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 sudo yum update -y
 
 sudo yum install -y vagrant VirtualBox-6.1
-#sudo /sbin/vboxconfig
+sudo /sbin/vboxconfig
 curl https://download.virtualbox.org/virtualbox/6.1.32/Oracle_VM_VirtualBox_Extension_Pack-6.1.32-149290.vbox-extpack --OUTPUT Oracle_VM_VirtualBox_Extension_Pack-6.1.32-149290.vbox-extpack
 sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.1.32.vbox-extpack
 sudo yum install -y gcc make perl kernel-devel kernel-devel-4.18.0-348.el8.0.2.x86_64
