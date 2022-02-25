c9cc91ae210a6068ffd1f1b6e952a3c23c54d078
Drathion <81193098+Drathion@users.noreply.github.com>
2022-02-25 01:57:01 -0500

    moved vboxconfig command to end - needs testing

--- rocky_vagranthost_setup.sh
+++ rocky_vagranthost_setup.sh
@@ -11,7 +11,7 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 sudo yum update -y
 
 sudo yum install -y vagrant VirtualBox-6.1
-sudo /sbin/vboxconfig
+
 curl https://download.virtualbox.org/virtualbox/6.1.32/Oracle_VM_VirtualBox_Extension_Pack-6.1.32-149290.vbox-extpack --OUTPUT Oracle_VM_VirtualBox_Extension_Pack-6.1.32-149290.vbox-extpack
 echo "y" | sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.1.32-149290.vbox-extpack
 sudo yum install -y gcc make perl kernel-devel kernel-devel-4.18.0-348.el8.0.2.x86_64
@@ -19,3 +19,5 @@ sudo yum install -y elfutils-libelf-devel
 sudo yum install -y docker-ce docker-ce-cli containerd.io
 
 echo "* 10.10.30.0/24" >> /etc/vbox/networks.conf
+
+sudo /sbin/vboxconfig
